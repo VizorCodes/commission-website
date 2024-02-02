818ab2cd087c43d34e31a08df60f224b42ca7111
VizorCodes <61893364+VizorCodes@users.noreply.github.com>
2024-02-01 20:11:20 -0800

    Delete Template #5 directory

--- Template #5/service.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function (){
-    $("#bustimg").hover(function(){
-        $("#bustimg").attr("src", "/Users/Destroyer/Desktop/Websites/CS_112_Main_Folder/Exercise 3/RawImages/headshot2.png");
-    }, function(){
-        $("#bustimg").attr("src", "/Users/Destroyer/Desktop/Websites/CS_112_Main_Folder/Exercise 3/RawImages/bust.png");
-    })
-})
